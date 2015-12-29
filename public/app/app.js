d46b868f7e3132489589e619677c2306641b579e
Jhirley <jhirley.fonte@gmail.com>
2015-12-29 17:54:54 -0500

    /admin/users working

--- public/app/app.js
+++ public/app/app.js
@@ -9,7 +9,10 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
 		.when('/', {
 			templateUrl: '/partials/main/main'
 			, controller: 'mvMainCtrl'
-		}
-	);
+		} )
+		.when('/admin/users', {
+			templateUrl: '/partials/admin/user-list'
+			, controller: 'mvUserListCtrl'
+		} );
 });
 
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var auth = require('./auth');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
 
 module.exports = function ( app ) {
+
+	app.get('/api/users', auth.requiresRole('admin'), function (req, res) {
+		User.find({}).exec(function (err, collection) {
+			res.send(collection);
+		});
+	});
+
 	app.get('/partials/*', function (req, res ) {
 		res.render('../../public/app/' + req.params[0]);
 	});
