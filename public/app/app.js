79d7e09ed3be60372bbc37e3528405862fb351a3
Jhirley <jhirley.fonte@gmail.com>
2015-12-30 17:53:13 -0500

    refactor and clean up

--- public/app/app.js
+++ public/app/app.js
@@ -3,6 +3,15 @@ angular.module('app', ['ngResource', 'ngRoute']);
 
 angular.module('app').config(function($routeProvider, $locationProvider) {
 
+	var routeRoleChecks = {
+		admin : {
+			auth: function (mvAuth) {
+				return mvAuth.authorizeCurrentUserForRoute('admin');					
+
+			}
+		}
+	}
+
 	$locationProvider.html5Mode(true);
 
 	$routeProvider
@@ -13,6 +22,15 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
 		.when('/admin/users', {
 			templateUrl: '/partials/admin/user-list'
 			, controller: 'mvUserListCtrl'
+			, resolve: routeRoleChecks.admin
 		} );
 });
 
+
+angular.module('app').run(function ($rootScope, $location) {
+	$rootScope.$on('$routeChangeError', function ( evt, current, previous, rejection) {
+		if ( rejection === 'not authorized') {
+			$location.path('/');
+		}
+	});
+});
