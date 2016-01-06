c0d5e51ce64beafa28dd5af4678d03597496bf5b
Jhirley <jhirley.fonte@gmail.com>
2016-01-06 12:48:14 -0500

    course list page complete

--- public/app/courses/mvCourseListCtrl.js
+++ public/app/courses/mvCourseListCtrl.js
@@ -1,4 +1,10 @@
 angular.module('app').controller('mvCourseListCtrl', function($scope, mvCourse) {
   $scope.courses = mvCourse.query();
   
+  $scope.sortOptions = [
+  	{value: "title", text:"Sort by Text"},
+  	{value: "published", text:"Sort by Date Published"}
+  	];
+  
+  $scope.sortOrder = $scope.sortOptions[0].value;
 });
