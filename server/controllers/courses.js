51aa5417ae5f38aeb107044908af648461de9ec1
Jhirley <jhirley.fonte@gmail.com>
2016-01-06 19:09:47 -0500

    mvCachedCourses added

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -3,5 +3,5 @@ var Course = require('mongoose').model('Course');
 exports.getCourses = function ( req, res ) {
 	Course.find({}).exec(function (err, collection) {
 		res.send(collection);
-	})
-}
+	});
+};
